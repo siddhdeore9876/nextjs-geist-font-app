605c761be50360e1347f8168c72df35f5d905df1
siddhdeore9876 <siddhesh4950@gmail.com>
2025-09-05 21:03:44 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1757086422846

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_58fd0e07._.js",
+  "static/chunks/src_76e6483a._.js"
+],
+    source: "dynamic"
+});
